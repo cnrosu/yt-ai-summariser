1536e47bfa7a1fcab87588e4e714020da1885437
cnrosu <103119670+cnrosu@users.noreply.github.com>
2025-06-07 19:46:54 +1000

    Remove server-side lzstring usage

--- chromeplugin/content.js
+++ chromeplugin/content.js
@@ -86,8 +86,13 @@ function loadCachedTranscript() {
     .then(data => {
       if (data.cached && data.transcript) {
         console.log("Cached transcript loaded.");
-        // Save transcript into chrome storage.
-        chrome.storage.local.set({ [`transcript_${videoId}`]: data.transcript }, () => {
+        let text = LZString.decompressFromBase64(data.transcript);
+        if (text === null) {
+          // Handle transcripts sent without compression
+          text = data.transcript;
+        }
+        const compressed = LZString.compressToUTF16(text);
+        chrome.storage.local.set({ [`transcript_${videoId}`]: compressed }, () => {
           console.log(`Transcript for video ${videoId} saved in chrome storage.`);
         });
         // Update UI: set button text and container background.
